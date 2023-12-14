1be4d524dcc22191f0c8188e0311609f9cdbbd8a
Apeksha Hatle <apekshahatle@gmail.com>
2023-12-15 02:13:33 +0530

    2nd commit

--- asg6.cpp
+++ asg6.cpp
@@ -1,212 +1,142 @@
-#include <iostream>
+#include<iostream>
+
 using namespace std;
 
-struct Node
+struct node
 {
-    struct Node *left,*right;
     int data;
-    
-    bool leftthread;
-    bool rightthread;
+    node *left;
+    node *right;
+
+    int lbit;
+    int rbit;
 };
 
-struct Node *Insert(struct Node*root, int key)
+node *root = NULL; 
+node *head=new node;
+void insert(node *root, node *temp)
 {
-    struct Node *node = root;
-    struct Node *par = NULL;
-    while(node != NULL)
-    {
-        if(key == (node->data))
+    // if(root==NULL)
+    // {return;}
+        if(temp->data<root->data)
         {
-            cout<<"Duplicate key"<<endl;
-            return root;
-        }
-        
-        par = node;
-        
-        if(key < (node->data))
-        {
-            if(node->leftthread == false)
+            if(root->lbit==0)
             {
-                node = node->left;
+                temp->left=root->left;
+                temp->right=root;
+                root->left=temp;
+                root->lbit=1;
             }
             else
-            break;
+            {
+                insert(root->left,temp);
+            }
+        }
+
+        else if(temp->data>root->data)
+        {
+            if(root->rbit==0)
+            {
+                temp->right=root->right;
+                temp->left=root;
+                root->right=temp;
+                root->rbit=1;
+            }
+            else
+            {
+                insert(root->right,temp);
+            }
+            
+        }
+
+}
+
+void create()
+{
+    char ch='y';
+    do{
+        int data;
+        cout<<"Enter element : ";
+        node *temp=new node;
+        cin>>data;
+        temp->data=data;
+        temp->right=NULL;
+        temp->left=NULL;
+        temp->lbit=0;
+        temp->rbit=0;
+
+        if(root==NULL)
+        {
+            root=temp;
+            head->data=999;         //can be anything you want
+            head->right=head;
+            head->left=root;
+            head->lbit=1;
+            head->rbit=1;
+            root->right=head;
+            root->left=head;
+            cout<<"you want to continue (y/n)? : ";
+            cin>>ch;
         }
         else
         {
-            if(node->rightthread == false)
-            {
-                node = node->right;
-            }
-            else
-            break;
+            insert(root,temp);
+            cout<<"you want to continue (y/n)? : ";
+            cin>>ch;
         }
-        
-    }
-    
-    struct Node *temp = new Node;
-    temp->data = key;
-    temp->leftthread = true;
-    temp->rightthread = true;
-    
-    if(par == NULL)
-    {
-        root = temp;
-        temp->left = NULL;
-        temp->right = NULL;
-    }
-    else if (key < (par -> data)) 
-    { 
-        temp -> left = par -> left; 
-        temp -> right = par; 
-        par -> leftthread = false; 
-        par -> left = temp; 
-    } 
-    else
-    { 
-        temp -> left = par; 
-        temp -> right = par -> right; 
-        par -> rightthread = false; 
-        par -> right = temp; 
-    } 
-  
-    return root; 
+
+    }while(ch=='y');
 }
 
-struct Node *InorderSuccessor(struct Node *node)
-{
-    if(node->rightthread == true)
-    {
-        return (node->right);
-    }
-    else
-    {
-        node = node->right;
-        while(node->leftthread == false)
-        {
-            node = node->left;
-        }
-    }
-    
-    return node;
-}
-void InorderTBT(struct Node *root)
-{
+void inorder() {
+    node *t=head->left;
 
-    if(root == NULL)
+    do{
+    while(t->lbit!=0)
     {
-        cout<<"Empty Tree"<<endl;
+        t=t->left;
     }
-    else
+    cout<<t->data<<" ";
+
+    while(t->rbit==0)
     {
-        struct Node *node = root;
-        
-        
-        while(node->leftthread == false)
-        {
-            node = node->left;
-        }
-        while(node != NULL)
-        {
-            cout<<" "<<node->data;
-            node = InorderSuccessor(node);
-        }
+        t=t->right;
+        if(t==head)
+            return;
+        cout<<t->data<<" ";
     }
+        t=t->right;
+    }while(t!=head);
+
 }
 
-void PreorderTBT(struct Node *root )  
-{  
-        struct Node *node;  
-        if(root == NULL)  
-        {  
-                cout<<"Empty Tree "<<endl;  
-                return;  
-        }
-        
-        node = root;  
-        while(node != NULL)  
-        {  
-                cout<<" "<<node->data; 
-                
-                if(node->leftthread == false)
-                {
-                        node=node->left;  
-                }        
-                else if(node->rightthread == false)  
-                {
-                        node = node->right; 
-                }
-                
-                else  
-                {  
-                        while(node != NULL && node->rightthread == true) 
-                        
-                                node = node->right;  
-                        if(node != NULL)  
-                                node = node->right;  
-                }  
-        }  
-}  
-int main()
+void preorder()
 {
-    struct Node *root = NULL;
-    int choice;
-    	
-    while(1)
-    {
- 
- 
-        cout<<"1.Insert "<<endl;
-        cout<<"2.InOrder Traversal "<<endl;
-        cout<<"3.Preorder Traversal"<<endl;
-        
-        
-        cout<<"Enter your choice : "<<endl;
-        cin>>choice;
- 	       
-        switch(choice)
+    node *t=head->left;
+
+    do{
+        while(t->lbit!=0)
         {
-            case 1:
-            {
-                cout<<"Enter the element you want to Insert : "<<endl;
-                cin>>choice;
-                root = Insert(root, choice);
-                
-            }
-            break;
-            
-            case 2:
-            {
-                cout<<"Inorder Traversal Of TBT : ";
-                InorderTBT(root);
-                cout<<endl;
-            }
-            break;
-            
-            case 3:
-            {
-                cout<<"Preorder Traversal Of TBT : ";
-                PreorderTBT(root);
-                cout<<endl;
-            }
-            break;
-            
-            case 4:
-            {
-                exit(0);
-                
-            }
-            break;
-            
-            default:
-            {
-                cout << "Invalid Choice." << endl;
-            }
-            break;
-   
+            cout<<t->data<<" ";
+            t=t->left;
         }
-        
-    }
-    return 0;  
+        cout<<t->data<<" ";
+        while(t->rbit==0)
+        {
+            t=t->right;
+            if(t==head)
+                return;
+        }
+        t=t->right;
+    }while(t!=head);
+}
+int main() {
+    int data;
+    create();
+    cout << "Inorder traversal: "<<endl;
+    inorder();
+    cout <<endl<< "Preorder traversal: "<<endl;
+    preorder();
+
+    return 0;
 }
